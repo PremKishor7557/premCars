380e5b9f5a684abfb3a84740db53a22e7b09c5da
PremKishor7557 <premshankarki69@gmail.com>
2025-12-27 12:04:46 +0530

    added admin and car booking page

--- server.js
+++ server.js
@@ -10,5 +10,15 @@ app.use(express.json())
 app.use('/api/cars/', carsRoute)
 app.use('/api/users/', usersRoute)
 app.use('/api/bookings/', bookingsRoute)
+
+const path = require('path')
+if(process.env.NODE_ENV === 'production')
+{
+    app.use('/', express.static('client/build'))
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+    })
+}
+
 app.get('/', (req, res)=> res.send('Hello World'))
 app.listen(port, ()=>console.log(`Node JS Server Started on Port ${port}`))
