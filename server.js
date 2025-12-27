d1bbbc66fdca44a411ae8ee4533cc493fe30364c
PremKishor7557 <premshankarki69@gmail.com>
2025-12-27 13:45:29 +0530

    removed if block

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.use('/api/users/', usersRoute)
 app.use('/api/bookings/', bookingsRoute)
 
 const path = require('path')
-if(process.env.NODE_ENV === 'production')
-{
+// if(process.env.NODE_ENV === 'production')
+// {
     app.use(express.static(path.join(__dirname, 'client/build')));
-    
+
     app.get(/.*/, (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build/index.html'));
     });
-}
+// }
 
 app.get('/', (req, res)=> res.send('Hello World'))
 app.listen(port, ()=>console.log(`Node JS Server Started on Port ${port}`))
